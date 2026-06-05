0e373fdfdd8893d95a1b54dbc1b60a40dc3bac86
lkashur <lkashur@gmail.com>
2026-06-05 10:24:22 -0700

    Add sentinel satellite layer

--- api/get-config.js
+++ api/get-config.js
@@ -1,6 +1,8 @@
 // api/get-config.js
 export default async function handler(req, res) {
   res.status(200).json({
-    mapTilerKey: process.env.MAPTILER_KEY
+    mapTilerKey: process.env.MAPTILER_KEY,
+    sentinelInstanceId: process.env.SENTINEL_INSTANCE_ID,
+    sentinelLayerId: process.env.SENTINEL_LAYER_ID
   });
 }
